7c0bba26937c158871b386ea08a50dc03b8c23a1
Anton Seitz <aseitz@puckit.de>
2022-11-07 09:59:03 +0100

    ADD SWITCHES -f --max and --throttle

--- erase_unused_space.sh
+++ erase_unused_space.sh
@@ -4,48 +4,59 @@ echo
 echo "DEACTIVATE BTRFS SNAPSHOTs for this folder!"
 echo "otherwise the system could get unstable and hang"
 echo
-read -p " REALLY START DISK FILLING ? (type REALLY to confirm) " key
-if [ "$key" !=  "REALLY" ];then
-exit
+if [ "$1" != "-f" ] ; then
+	read -p " REALLY START DISK FILLING ? (type REALLY to confirm) " key
+	if [ "$key" !=  "REALLY" ];then
+		exit
+	fi
 fi
 
 
-read -p " GO WIth maximum dd speed ? (type max to do so ) " speed
-if [ "$speed" ==  "max" ];then
-	BS=8192
-	echo "Ok, go for max speed"
+
+if [ "$2" != "--max" ] && [ "$2" != "--throttle" ]; then
+	read -p " GO WIth maximum dd speed ? (type max to do so ) " speed
+	if [ "$speed" ==  "max" ];then
+		BS=8192
+		echo "Ok, go for max speed"
+	else
+		BS=256
+		echo "Default: throttled speed"
+	fi
 else
-	BS=256
-	echo "Default: throttled speed"
+	if [ "$2" == "--max" ];then
+		BS=8192
+	fi
+	if [ "$2" == "--throttle" ]; then
+		BS=256
+	fi
 fi
-
 
 echo "creting small file... "
 if [ -f zerofill.small.file ]; then
-echo "alredy present!"
+	echo "alredy present!"
 else
-dd if=/dev/zero of=zerofill.small.file bs=1024 count=2024000 status=progress
+	dd if=/dev/zero of=zerofill.small.file bs=1024 count=2024000 status=progress
 fi
 
 echo " fill disk without limit untill it is full"
 for (( x=11; x+=1 ; x>0)); do 
 if [ -f zerofill.big.fill.$x ] ;then 
-echo "zerofill.big.fill.$x already exits; skipping"
-continue
+	echo "zerofill.big.fill.$x already exits; skipping"
+	continue
 else
 
-echo "create zerofill.big.fill.$x.."
-dd if=/dev/zero of=zerofill.big.fill.$x bs=$BS status=progress count=100000000
-LASTCODE=$?
-echo $LASTCODE
-if [ $LASTCODE -gt 0 ]; then 
-echo "DISK FULL: exit loop"
-break;
+	echo "create zerofill.big.fill.$x.."
+	dd if=/dev/zero of=zerofill.big.fill.$x bs=$BS status=progress count=100000000
+	LASTCODE=$?
+	echo $LASTCODE
+	if [ $LASTCODE -gt 0 ]; then 
+		echo "DISK FULL: exit loop"
+		break;
+	else
+		echo "..created! "
+	fi
+fi
 
-else
-echo "..created! "
-fi
-fi
 done
 sync
 
